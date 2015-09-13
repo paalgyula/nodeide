89deda6f9a447bdbb7b819554e933f2bf2ae7e36
Paál Gyula <paalgyula@paalgyula.com>
2015-09-13 23:03:07 +0200

    Added settings manager based stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,31 @@
 #include "src/mainwindow.h"
 #include <QApplication>
+#include <qsplashscreen.h>
+#include <QDesktopWidget>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    QApplication app(argc, argv);
+    QPixmap pixmap(":/icons/logo.png");
+    QSplashScreen splash(pixmap);
+    splash.show();
 
-    return a.exec();
+    // Process events :)
+    app.processEvents();
+
+    QCoreApplication::setOrganizationName("paalgyula");
+    QCoreApplication::setOrganizationDomain("paalgyula.com");
+    QCoreApplication::setApplicationName("NodeIDE");
+
+    MainWindow w;
+    splash.finish(&w);
+
+    w.setGeometry(QStyle::alignedRect(
+            Qt::LeftToRight,
+            Qt::AlignCenter,
+            w.size(),
+            app.desktop()->availableGeometry()));
+
+    w.showMaximized();
+    return app.exec();
 }
